6dbf8d30e40a0c803e029321896a4654b5d92370
Verinikita <80643476+Verinikita@users.noreply.github.com>
2024-01-19 18:54:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,25 +7,28 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './downlo'
 
 ALLOWED_EXTENSIONS = {'zip'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def AlphaFoldXploR_read(folder):
+    for name, handle in folder:
+        if name.endswith(".json"):
+            with handle as file:
+                lista1 = file.read_text()
+                # Procesa el contenido del archivo JSON según tus necesidades
+
+        if name.endswith(".pdb"):
+            with handle as file:
+                lista2 = file.read_text()
+                # Procesa el contenido del archivo PDB según tus necesidades
+
 @app.route('/procesar_zip', methods=['POST'])
 def procesar_zip():
     zip_file = request.data  # El contenido del archivo .zip se encuentra en request.data
-return 'OK', 200 
-def AlphaFoldXploR_read(zfile):
-    with ZipFile(zfile, 'r') as fz:
-        for zip_info in fz.infolist():
-            if zip_info.filename[-1] == '/':
-                continue
-            tab = os.path.basename(zip_info.filename)
-            if tab.endswith(".json"):
-                zip_info.filename = os.path.basename(zip_info.filename)
-                lista1 = fz.extract(zip_info, "archivos_json")
+    # Llama a tu función de procesamiento de archivos .zip aquí
+    AlphaFoldXploR_read(zip_file)
+    return 'OK', 200
 
-    with ZipFile(zfile, 'r') as fz:
-        for zip_info in fz.infolist():
-            if zip_info.filename[-1] == '/':
-                continue
-            tab = os.path.basename(zip_info.filename)
-            if tab.endswith(".pdb"):
-                zip_info.filename = os.path.basename(zip_info.filename)
-                lista2 = fz.extract(zip_info, "archivos_pdb")
+if __name__ == '__main__':
+    app.run(debug=True)
